81519f6afcdc2c495f4a2a81f98bcb99b6f58e2f
RikkeFisker <riand25@student.sdu.dk>
2026-08-31 16:03:05 +0200

    Oprettelse af Models-mappe og Costumer.cs

---
+++ AIRSUPPORT/Components/Models/Costumer.cs
@@ -0,0 +1,12 @@
+﻿namespace AIRSUPPORT.Components.Models
+{
+    public class Costumer
+    {
+        public string ID { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Country { get; set; } = "";
+        public Boolean Churn { get; set; }; 
+
+    }
+}
