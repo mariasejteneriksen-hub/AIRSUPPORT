5e6426a48f6660e73caa7d1a7c0d5b177057d079
RikkeFisker <riand25@student.sdu.dk>
2026-08-31 16:18:43 +0200

    Rettet til CurrentCostumer

--- AIRSUPPORT/Components/Models/Costumer.cs
+++ AIRSUPPORT/Components/Models/Costumer.cs
@@ -1,12 +1,12 @@
 ﻿namespace AIRSUPPORT.Components.Models
 {
-    public class Costumer
+    public class CurrentCostumer
     {
-        public string ID { get; set; } = "";
-        public string Name { get; set; } = "";
-        public string Status { get; set; } = "";
-        public string Country { get; set; } = "";
-        public Boolean Churn { get; set; }; 
+        public string ID { get; set; };
+        public string Name { get; set; };
+        public string Status { get; set; };
+        public string Country { get; set; };
+        
 
     }
 }
