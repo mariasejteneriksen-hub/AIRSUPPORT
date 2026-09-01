68dcc63b6a24474e20d4e96cfc4f9dcb71aae9a6
RikkeFisker <riand25@student.sdu.dk>
2026-09-01 07:20:06 +0200

    Viderearbejde med modellerne CurrentCostumer og FormerCostumer

---
+++ AIRSUPPORT/Components/Models/CurrentCostumer.cs
@@ -0,0 +1,16 @@
+﻿namespace AIRSUPPORT.Components.Models
+{
+    public class CurrentCostumer
+    {
+        public string? ID { get; set; }
+        public string? Name { get; set; }
+        public string? Status { get; set; }//overvej at slette, fordi de burde jo være current 
+        public string? Country { get; set; }
+        public DateTime? CostumerSince { get; set; }
+        public string? CancelTerms { get; set; }//tjek om der er mange naan-værdier
+        public int? PriceEscalation { get; set; }//tjek om der er mange naan-værdier
+
+
+    }
+
+    }
---
+++ AIRSUPPORT/Components/Models/FormerCostumer.cs
@@ -0,0 +1,12 @@
+﻿namespace AIRSUPPORT.Components.Models
+{
+    public class FormerCostumer : CurrentCostumer
+    {
+        public DateTime? ChurnDate { get; set; }
+        public int? ChurnValue { get; set; }
+        public string? ChurnReason { get; set; }
+        public Boolean? Avoidable { get; set; }
+
+
+    }
+}
